cc20056557d7d1386088f9346ea06167281e12e7
cumelmell <cumelmell@foxmail.com>
2019-11-23 22:04:41 +0800

    feat(TDD/vue-test): UndoList add css

--- charter4/vue-test/src/App.vue
+++ charter4/vue-test/src/App.vue
@@ -19,4 +19,6 @@ export default {
 *
   margin 0
   padding 0
+body
+  background #cdcdcd
 </style>
--- charter4/vue-test/src/containers/TodoList/components/UndoList.vue
+++ charter4/vue-test/src/containers/TodoList/components/UndoList.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
-    <div data-test="count">{{ list.length }}</div>
-    <ul>
+  <div class="undo-list">
+    <div class="title">
+      正在进行:
+      <span data-test="count" class="count">{{ list.length }}</span>
+    </div>
+    <ul class="list">
       <li
         v-for="(item, index) in list"
         :key="item"
         data-test="item"
+        class="item"
       >
         {{ item }}
-        <span data-test="delete-btn" @click="handleDelete(index)">-</span>
+        <span data-test="delete-btn" @click="handleDelete(index)" class="delete">-</span>
       </li>
     </ul>
   </div>
@@ -27,5 +31,42 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.undo-list
+  width 600px
+  margin 0 auto
+  .title
+    line-height 30px
+    font-size 24px
+    font-weight 600
+    margin 10px 0
 
+  .count, .delete
+    float right
+    display block
+    width 20px
+    height 20px
+    line-height 20px
+    text-align center
+    background #e6e6e6
+    border-radius 10px
+    margin-top 5px
+    font-size 12px
+    color #000
+
+  .delete
+    text-indent 0
+    margin-right 10px
+    cursor pointer
+
+  .list
+    list-style none
+
+  .item
+    margin-bottom 10px
+    text-indent 20px
+    line-height 32px
+    font-size 14px
+    background #fff
+    border-left 5px solid #629a9a
+    border-radius 3px
 </style>
